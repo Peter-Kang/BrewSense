3207d1ad4160a165b89a78c34892e1f585e2704e
Peter-Kang <pkang93@gmail.com>
2023-10-13 16:08:17 -0400

    Updated clouds

--- src/Frontend/src/App.tsx
+++ src/Frontend/src/App.tsx
@@ -13,7 +13,6 @@ function App() {
       <div className="cloud"></div>
       <div className="cloud"></div>
       <div className="cloud"></div>
-      <div className="cloud"></div>
     </>
   )
 }
