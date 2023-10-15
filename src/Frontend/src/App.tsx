152e232dbe7769f50556cd73bb42a74ea6cbc721
Peter-Kang <pkang93@gmail.com>
2023-10-14 23:40:07 -0400

    Added a card and layered the background clouds

--- src/Frontend/src/App.tsx
+++ src/Frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Clouds from './components/cloud/cloud'
+import MainContent from './components/mainContent/mainContent'
 
 function App() {
   return (
     <>
      <Clouds />
+     <MainContent/>
     </>
   )
 }
--- src/Frontend/src/components/mainContent/mainContent.tsx
+++ src/Frontend/src/components/mainContent/mainContent.tsx
@@ -1,11 +1,17 @@
+import "./mainContent.css"
 
-const mainContent =  () =>
+const MainContent =  () =>
 {
 return (
     <>
-    
+    <div className="card">
+        <img src="https://doodleipsum.com/500x500/hand-drawn?i=b48528b59f8d071fde57e51d2d6c433d" alt="Card image cap"/>
+        <h5>Lorem ipsum dolor</h5>
+        <p>Quis nostrud nisi ut aliquip ea commodo consequat.</p>
+        <a href="#">Check it</a>
+    </div>
     </>
 );
 }
 
-export default mainContent;
+export default MainContent;
